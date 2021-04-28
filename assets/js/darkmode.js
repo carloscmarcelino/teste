4b823d4154018535c7b6b9f88aaa3d62a9d9ff89
Carlos Cesar <76910462+carloscmarcelino@users.noreply.github.com>
2021-04-28 11:03:47 -0300

    Add files via upload

---
+++ assets/js/darkmode.js
@@ -0,0 +1,33 @@
+const html = document.querySelector("html")
+const checkbox = document.querySelector("input[name=theme]")
+
+const getStyle = (element, style) => 
+    window
+        .getComputedStyle(element)
+        .getPropertyValue(style)
+
+
+const initialColors = {
+    bg: getStyle(html, "--bg"),
+    bgPanel: getStyle(html, "--bg-panel"),
+}
+
+const darkMode = {
+    bg: "#5909ad",
+    bgPanel: "#7c12ed",
+}
+
+const transformKey = key => 
+    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+
+
+const changeColors = (colors) => {
+    Object.keys(colors).map(key => 
+        html.style.setProperty(transformKey(key), colors[key]) 
+    )
+}
+
+
+checkbox.addEventListener("change", ({target}) => {
+    target.checked ? changeColors(darkMode) : changeColors(initialColors)
+})
