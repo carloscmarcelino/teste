72e4511445aa761d964146d1c925b40b2bd9d2cb
Carlos Cesar <carloslima1598@gmail.com>
2021-05-14 21:35:53 -0300

    add files

---
+++ assets/projetos/porpino-arquitetura/assets/js/script.js
@@ -0,0 +1,28 @@
+/*Scroll-top*/
+const menuItens = document.querySelectorAll('header a[href^="#"]')
+menuItens.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick)
+})
+function getScrollTopByHref(element){
+    const id = element.getAttribute('href')
+    return document.querySelector(id).offsetTop
+}
+function scrollToIdOnClick(event){
+    event.preventDefault()
+    const to = getScrollTopByHref(event.target)
+    scrollToPosition(to)
+}
+function scrollToPosition(to){
+    window.scroll({
+        top: to,
+        behavior: "smooth",
+    })
+}
+
+
+/*Menu-mobile*/
+const btnMobile = document.querySelector('#btn-mobile')
+btnMobile.addEventListener('click', function(){
+    const menu = document.querySelector('.menu')
+    menu.classList.toggle('active')
+})
---
+++ assets/projetos/porpino-arquitetura/readme.md
@@ -0,0 +1,1 @@
+### Desenvolvendo um site para um amigo.
