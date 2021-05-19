65fb7554c75a7a84496f21b671012807158f7dc4
Carlos Cesar <carloslima1598@gmail.com>
2021-05-19 00:02:46 -0300

    update

--- assets/projetos/nery-tatoo/assets/js/script.js
+++ assets/projetos/nery-tatoo/assets/js/script.js
@@ -1,25 +1,3 @@
-/*Slide-home*/
-function slide(){
-let time = 5000,
-    currentImageIndex = 0,
-    imagens = document.querySelectorAll('#home img')
-    max = imagens.length;
-function nextImage(){
-    imagens[currentImageIndex].classList.remove('ativo')
-    currentImageIndex++
-    if(currentImageIndex >= max){
-        currentImageIndex = 0
-    }
-    imagens[currentImageIndex].classList.add('ativo')
-}
-function start(){
-    setInterval(() => {
-        nextImage()
-    }, time)
-}
-window.addEventListener("load", start())
-}
-slide()
 /*Scroll-top*/
 const menuItens = document.querySelectorAll('header a[href^="#"]')
 menuItens.forEach(item => {
@@ -32,7 +10,7 @@ function getScrollTopByHref(element){
 function scrollToIdOnClick(event){
     event.preventDefault()
     menuHeight = $('header').innerHeight()
-    const to = getScrollTopByHref(event.target) - menuHeight
+    const to = getScrollTopByHref(event.target)
     scrollToPosition(to)
 }
 function scrollToPosition(to){
@@ -42,15 +20,6 @@ function scrollToPosition(to){
     })
 }
 /*Scroll-reveal*/
-/*Top*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '50px',
-    duration: 2000,
-    reset: true
-});
-sr.reveal('.titulo-delay',{delay: 200})
-sr.reveal('.cards-interval',{interval: 400})
 /*Left*/
 const srl = ScrollReveal({
     origin: 'left',
@@ -60,11 +29,3 @@ const srl = ScrollReveal({
 });
 srl.reveal('.titulo-delayLeft',{delay: 200})
 srl.reveal('.cards-intervalLeft',{interval: 400})
-/*No-reset*/
-const srNR = ScrollReveal({
-    origin: 'top',
-    distance: '50px',
-    duration: 2000,
-    reset: false
-});
-sr.reveal('.cards-intervalNR',{interval: 400})
