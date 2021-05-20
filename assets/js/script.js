3df39ebb89079241ebf65771541aef96f2d7c702
Carlos Cesar <carloslima1598@gmail.com>
2021-05-20 09:03:34 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,7 +80,7 @@ srl.reveal('.titulo-delayLeft',{delay: 200})
 srl.reveal('.cards-intervalLeft',{interval: 400})
 /*No-reset*/
 const srNR = ScrollReveal({
-    origin: 'left',
+    origin: 'top',
     distance: '50px',
     duration: 2000,
     reset: false
