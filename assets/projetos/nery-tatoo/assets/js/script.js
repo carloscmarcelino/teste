858c0a216335264508d46dbd7fc95728e1c28ffd
Carlos Cesar <carloslima1598@gmail.com>
2021-06-07 19:23:21 -0300

    add nery-tatoo

---
+++ assets/projetos/nery-tatoo/assets/js/script.js
@@ -0,0 +1,9 @@
+/*Slide*/
+import Slide from './modules/slide.js';
+const slide = new Slide('.slide', '.slide-wrapper');
+slide.init();
+slide.changeSlide(0);
+slide.activePrevSlide();
+/*Header-animation*/
+import Header from './modules/header.js'
+Header()
