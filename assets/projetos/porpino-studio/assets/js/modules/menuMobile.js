3c3c9f8a6a7a6b1b6449bf6c305bd8b24e72f069
Carlos Cesar <carloslima1598@gmail.com>
2021-06-16 11:22:24 -0300

    atualizando projetos

--- assets/projetos/porpino-studio/assets/js/modules/menuMobile.js
+++ assets/projetos/porpino-studio/assets/js/modules/menuMobile.js
@@ -1,7 +1,7 @@
 export default function menuMobile(){
     const btnMobile = document.querySelector('#btn-mobile')
     btnMobile.addEventListener('click', function(){
-        const menu = document.querySelector('.menu')
+        const menu = document.querySelector('header')
         menu.classList.toggle('active')
     })
 }
