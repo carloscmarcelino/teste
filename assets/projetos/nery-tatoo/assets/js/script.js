cfd6ff9db8fef1b527712b3606780a7d5bc1fcf6
Carlos Cesar <carloslima1598@gmail.com>
2021-06-18 09:46:51 -0300

    Update projects

--- assets/projetos/nery-tatoo/assets/js/script.js
+++ assets/projetos/nery-tatoo/assets/js/script.js
@@ -1,13 +1,18 @@
 /*Slide*/
-import Slide from './modules/slide.js';
-const slide = new Slide('.slide', '.slide-wrapper');
+import {SlideNav} from './modules/slide.js';
+const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
+slide.addArrow('.prev', '.next');
+/*
 slide.changeSlide(0);
 slide.activePrevSlide();
+*/
+
 
 /*Header-animation*/
 import Header from './modules/header.js'
 Header()
+
 
 /*menuMobile*/
 import menuMobile from './modules/menuMobile.js';
