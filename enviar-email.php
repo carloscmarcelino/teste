0a2d023c36b7c41cc8ab68bc6b1b9f8065ccf004
Carlos Cesar <carloslima1598@gmail.com>
2021-06-24 20:52:52 -0300

    teste form php

---
+++ enviar-email.php
@@ -0,0 +1,43 @@
+<?php 
+
+    $nome = utf8_encode($POST['nome']);
+    $email = utf8_encode($POST['email']);
+    $mensagem = utf8_encode($POST['mensagem']);
+
+    require 'PHPMailer/PHPMailerAutoload.php';
+
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+
+    //Config server
+    $mail->Host = "smtp.gmail.com";
+    $mail->Port = "587";
+    $mail->SMTPSecure = "TLS";
+    $mail->SMTPAuth = "true";
+    $mail->Username = "carlosdev@gmail.com";
+    $mail->Password = "1598karlos";
+
+    $mail->setFrom($mail->Username"Carlos Cesar");//remetente
+    $mail->addAddress('carlosdev@gmail.com');//destinatario
+    $mail->Subject = "Fale conosco";
+
+    $conteudo_email = 
+    "
+        Você recebeu uma mensagem de $nome, $email, 
+        <br>
+        Mensagem: 
+        $mensagem
+    ";
+
+    $mail->IsHTML(true);
+    $mail->Body = $conteudo_email;
+
+    if ( $mail->send ){
+        echo "
+                Formulário enviado com sucesso
+                Em breve eu entro em contato com você.
+             "
+    } else{
+        echo "Falha ao enviar o contato." . mail->ErrorInfo;
+    }
+?>
