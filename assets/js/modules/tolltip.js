546d7d8a5e567c0029de22dd3e2ffd37a1733a23
Carlos Cesar <carloslima1598@gmail.com>
2021-07-22 16:46:15 -0300

    update tooltip

--- assets/js/modules/tolltip.js
+++ assets/js/modules/tolltip.js
@@ -1,12 +1,5 @@
 import $ from 'jquery';
 
 export default function tolltip() {
-  $(".tolltip").on('mouseover', function () {
-    let mouse = event.pageX;
-    $(this).append("<style>.tolltip::after { left: " + mouse + "px; }</style>")
-  });
-
-  $(".tolltip").on('mouseout', function () {
-    $('.tolltip style').remove();
-  })
+  
 }
