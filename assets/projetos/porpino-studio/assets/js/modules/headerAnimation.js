eb63866d9d5ef07ec9c6859d3ae0f167ad1a9919
Carlos Cesar <carloslima1598@gmail.com>
2021-07-22 10:37:53 -0300

    update projects

--- assets/projetos/porpino-studio/assets/js/modules/headerAnimation.js
+++
@@ -1,6 +0,0 @@
-export default function headerAnimation(){
-    window.addEventListener('scroll', function(){
-        const header = document.querySelector('header')
-        header.classList.add('hd-Animated', window.scroll > 0)
-    })
-}
--- assets/projetos/porpino-studio/assets/js/modules/menuMobile.js
+++
@@ -1,7 +0,0 @@
-export default function menuMobile(){
-    const btnMobile = document.querySelector('#btn-mobile')
-    btnMobile.addEventListener('click', function(){
-        const menu = document.querySelector('header')
-        menu.classList.toggle('active')
-    })
-}
