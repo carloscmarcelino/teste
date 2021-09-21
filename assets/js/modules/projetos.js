1d0cb0d55ef4ebfb80124f038f7cfd755685ae5e
Carlos C Marcelino <carloslima1598@gmail.com>
2021-09-21 10:29:31 -0300

    update

--- assets/js/modules/projetos.js
+++ assets/js/modules/projetos.js
@@ -1,9 +1,9 @@
 export default function projetos() {
   const largura = innerWidth;
-  if ( largura > 500 ) {
+  if ( largura > 1400 ) {
     const projetos = document.querySelectorAll('.design');
     projetos.forEach((i) => {
-      i.classList.remove('hidden')
+      i.classList.remove('hidden');
     })
   }
 }
